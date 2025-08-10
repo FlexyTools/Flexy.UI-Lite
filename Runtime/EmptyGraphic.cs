5acd78ca63ca8ef1e0553a33d210f94f1200b4db
JesOb <JesObertyukh@gmail.com>
2025-08-10 19:28:40 +0200

    Added Empty Graphic and UI Gradient

---
+++ Runtime/EmptyGraphic.cs
@@ -0,0 +1,29 @@
+﻿namespace Flexy.UI
+{
+    /// A concrete subclass of the Unity UI `Graphic` class that just skips drawing.
+    /// Useful for providing a raycast target without actually drawing anything.
+    [RequireComponent(typeof(CanvasRenderer))]
+    public class EmptyGraphic : Graphic
+    {
+        public override		void	SetMaterialDirty	( ) { }
+        public override		void	SetVerticesDirty	( ) { }
+
+        /// Probably not necessary since the chain of calls `Rebuild()`->`UpdateGeometry()`->`DoMeshGeneration()`->`OnPopulateMesh()` won't happen; so here really just as a fail-safe.
+        protected override	void	OnPopulateMesh		( VertexHelper vh ) { vh.Clear(); }
+    }
+    
+    #if UNITY_EDITOR
+    [UnityEditor.CanEditMultipleObjects, UnityEditor.CustomEditor( typeof(EmptyGraphic), false )]
+    public class NonDrawingGraphicEditor : UnityEditor.UI.GraphicEditor
+    {
+	    public override void OnInspectorGUI ( )
+	    {
+		    serializedObject.Update();
+		    //UnityEditor.EditorGUILayout.PropertyField( m_Script );
+		    // skipping AppearanceControlsGUI
+		    RaycastControlsGUI();
+		    serializedObject.ApplyModifiedProperties();
+	    }
+    }
+    #endif
+}
---
+++ Runtime/UIGradient.cs
@@ -0,0 +1,63 @@
+﻿namespace Flexy.UI
+{
+	public class UIGradient : BaseMeshEffect 
+	{
+		[SerializeField]	Color		_topLeft 		= Color.white;
+		[SerializeField]	Color		_topRight 		= Color.white;
+		[SerializeField]	Color		_bottomLeft 	= Color.white;
+		[SerializeField]	Color		_bottomRight 	= Color.white;
+		[Range(-180f, 180f)]
+		[SerializeField]	Single		_angle			= 0f;
+		[SerializeField]	Boolean		_ignoreRatio	= true;
+
+		public override		void		ModifyMesh		( VertexHelper vh )		
+		{
+			if (!enabled) 
+				return;
+			
+			var rect	= graphic.rectTransform.rect;
+			var dir		= RotationDir( _angle * -1 );
+
+			if (!_ignoreRatio)
+			{
+				var ratio = rect.height / rect.width;
+				dir.x *= ratio;
+				dir = math.normalize( dir );
+			}
+			
+			var rotationMatrix = new float2x2( dir.xy, dir.yx );
+
+			var vertex = default(UIVertex);
+			for (var i = vh.currentVertCount - 1; i >= 0; i--) 
+			{
+				vh.PopulateUIVertex	( ref vertex, i );
+
+				var localPosition	= math.mul( rotationMatrix, Normalize(rect, vertex.position) );
+				vertex.color		*= QuadLerp(_topLeft, _topRight, _bottomLeft, _bottomRight, localPosition / 2f + 0.5f);
+				
+				vh.SetUIVertex		( vertex, i );
+			}
+		}
+		
+		private static		float2		RotationDir		( Single angle )										
+		{
+			var angleRad = angle * Mathf.Deg2Rad;
+			return new Vector2( math.cos( angleRad ), math.sin( angleRad ) );
+		}
+		private static		float2		Normalize		( in Rect rect, float3 vertexPos )						
+		{
+			var pos = vertexPos.xy;
+			pos.x = (pos.x - rect.xMin)/rect.width * 2 - 1;
+			pos.y = (pos.y - rect.yMin)/rect.height * 2 - 1;
+			pos.y *= -1;
+			
+			return pos;
+		}
+		private static		Color		QuadLerp		( Color tl, Color tr, Color bl, Color br, Vector2 t )	
+		{
+			var a =	Color.LerpUnclamped( tl, tr, t.x );
+			var b =	Color.LerpUnclamped( bl, br, t.x );
+			return	Color.LerpUnclamped( a, b, t.y );
+		}
+	}
+}
