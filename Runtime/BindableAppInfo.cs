abd515bd3f00e5747f5363dc3ad081f0f7064096
JesOb <JesObertyukh@gmail.com>
2025-10-29 21:39:38 +0100

    Added AppInfo VersionTag and Branch info properties

--- Runtime/BindableAppInfo.cs
+++ Runtime/BindableAppInfo.cs
@@ -8,7 +8,12 @@ namespace Flexy.UI
 		[Bindable] public	String			AppBundle			=> Application.identifier;
 		[Bindable] public	String			AppCompany			=> Application.companyName;
 		[Bindable] public	String			AppVersion			=> Application.version;
-		[Bindable] public	String			AppRev				=> AppRevision.AppRev;
-		[Bindable] public	String			AppRevShort			=> AppRevision.AppRevShort;
+		[Bindable] public	String			AppRev				=> AppInfo.AppRev;
+		[Bindable] public	String			AppRevShort			=> AppInfo.AppRevShort;
+		[Bindable] public	String			AppBranch			=> AppInfo.AppBranch;
+		[Bindable] public	String			AppVersionTag		=> AppInfo.AppVersionTag;
+		
+		[Bindable] public	String			AppTagAndVersion	=> AppInfo.AppVersionTagAndVersion;
+		[Bindable] public	String			AppRevAndBranch		=> AppInfo.RevisionAndBranch;
 	}
 }
