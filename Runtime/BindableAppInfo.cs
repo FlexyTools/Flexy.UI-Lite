3d0324d13e08fc30768e0707212a108fb3d8c9ce
JesOb <JesObertyukh@gmail.com>
2025-10-29 07:47:56 +0100

    Changed BindableAppInfo to read revision from AppRevision static class

--- Runtime/BindableAppInfo.cs
+++ Runtime/BindableAppInfo.cs
@@ -1,15 +1,14 @@
+using Flexy.AssetRefs.Extra;
+
 namespace Flexy.UI
 {
 	public class BindableAppInfo : BindableBehaviour
 	{
-		private String? _cachedAppRev;
-		
 		[Bindable] public	String			AppName				=> Application.productName;
 		[Bindable] public	String			AppBundle			=> Application.identifier;
 		[Bindable] public	String			AppCompany			=> Application.companyName;
 		[Bindable] public	String			AppVersion			=> Application.version;
-		[Bindable] public	String			AppRev				=> _cachedAppRev ??= Resources.Load<TextAsset>( "Fun.Flexy/BuildArtifacts/Revision" ) is { text: not null } o ? o.text : "";
-		
-		public				void			SetAppRevision		( String revision ) => _cachedAppRev = revision;
+		[Bindable] public	String			AppRev				=> AppRevision.AppRev;
+		[Bindable] public	String			AppRevShort			=> AppRevision.AppRevShort;
 	}
 }
